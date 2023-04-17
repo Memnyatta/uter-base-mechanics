a5ee2180d103ce757d9e1212f00fd636a8d97440
Memnyatta <shumaylov2000@gmail.com>
2023-04-17 15:05:25 +0300

    test bullet

--- Assets/Characters/turret/scripts/bulletMove.cs
+++ Assets/Characters/turret/scripts/bulletMove.cs
@@ -17,12 +17,12 @@ public class bulletMove : MonoBehaviour
     }
     public IEnumerator startMove(float speed,float time,Vector3 dir) 
     {
-        //Debug.Log("1111");
+        Debug.Log("startMove");
         nextTime = Time.time + time;
-        while (Time.time < nextTime) 
-        {
-            charCont.Move(dir * speed * Time.deltaTime);
-        }
+        
+        for (int i = 0; i < time; i++) { charCont.Move(dir * speed * Time.deltaTime); }
+        
+        
         
         yield return null;
     }
