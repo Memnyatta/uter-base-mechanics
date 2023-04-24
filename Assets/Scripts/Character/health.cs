de2f3dfd8908cbb3249d525a1d70cbdc9d62feae
Memnyatta <shumaylov2000@gmail.com>
2023-04-24 21:46:58 +0300

    base health system

--- Assets/Scripts/Character/health.cs
+++ Assets/Scripts/Character/health.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float pubmaxHealth;
+    [Header("Для просмотра")]
+    public float pubcurHealth;
+    
+    ////////////////////
+    
+    public float maxHealth;
+    public float curHealth;
+    public void setPub() 
     {
-        
+        maxHealth = pubmaxHealth;
+        //curHealth = pubcurHealth;
+    }
+    public void dealDamage(float damage, string source)  //My source is what i made it the fuck up
+    {
+        curHealth -= damage;
+        if (curHealth <= 0) { death(); }
+    }
+    public void dealHeal(float heal, string source)  
+    {
+        curHealth += heal;
+        if (curHealth > maxHealth) { curHealth = maxHealth; }
+    }
+    public void death() 
+    {
+    
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        setPub();
+        curHealth = maxHealth;
+
+        dealHeal(1, "self");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        pubcurHealth = curHealth;
     }
 }
