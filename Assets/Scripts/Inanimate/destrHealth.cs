1d44c1f186e283c3492e44a91100ed1f793137c1
Memnyatta <shumaylov2000@gmail.com>
2023-06-22 17:45:34 +0300

    Almost made destroyable object

--- Assets/Scripts/Inanimate/destrHealth.cs
+++ Assets/Scripts/Inanimate/destrHealth.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class destrHealth : health
 {
-    // Start is called before the first frame update
-    void Start()
+    public float blastForce;
+    public float disappearTime;
+    [Header("Для просмотра")]
+    public MeshRenderer meshRend;
+    public Collider col;
+    public GameObject allPieces;
+    public List<Rigidbody> pieces;
+
+    public IEnumerator waitForDest(float time)
     {
-        
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+        yield return null;
+    }
+    void Awake()
+    {
+        col = GetComponent<BoxCollider>();
+        meshRend = GetComponent<MeshRenderer>();
+        foreach (Transform t in allPieces.transform) 
+        {
+            pieces.Add(t.GetComponent<Rigidbody>());
+            //Debug.Log(t.gameObject.name);
+        }
+        allPieces.SetActive(false);
+        setPub();
+        curHealth = maxHealth;
     }
     public override void death()
     {
+        meshRend.enabled = false;
+        col.enabled = false;
+        allPieces.SetActive(true);
+        foreach (Rigidbody r in pieces) 
+        {
+            r.AddForce(Vector3.up * blastForce);
+        }
         Debug.Log(gameObject.name + " destroyed");
+        waitForDest(disappearTime);
     }
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        pubcurHealth = curHealth;
+        pubisInvincible = isInvincible;
+        //Debug.Log("fixedUpdate");
     }
 }
--- Assets/Scripts/damageOnTrigger.cs
+++ Assets/Scripts/damageOnTrigger.cs
@@ -26,7 +26,7 @@ public class damageOnTrigger : MonoBehaviour
     {
         if (onCol != null)
         {
-            Debug.Log("Damage trigger: " + gameObject.name + " collided with " + other.gameObject.name);
+           //Debug.Log("Damage trigger: " + gameObject.name + " collided with " + other.gameObject.name);
             onCol(gameObject);
             //onCol = null;
         }
@@ -37,7 +37,7 @@ public class damageOnTrigger : MonoBehaviour
     {
         curDam = null;
         curDam = obj.GetComponent<IDamageable>();
-        Debug.Log("Damage trigger: " + gameObject.name + " damaged " + obj.name);
+       // Debug.Log("Damage trigger: " + gameObject.name + " damaged " + obj.name);
         if (curDam == null) return;
         curDam.dealDamage(damage, gameObject.name);
     }
@@ -54,7 +54,7 @@ public class damageOnTrigger : MonoBehaviour
         //Debug.Log(other.gameObject);
         if (canBeDest && other.gameObject != gameObject) 
         {
-            Debug.Log(other.gameObject.tag + other.gameObject.name);
+            //Debug.Log(other.gameObject.tag + other.gameObject.name);
             if (tags.Contains(other.gameObject.tag) && canBeDest)
             {
                 
