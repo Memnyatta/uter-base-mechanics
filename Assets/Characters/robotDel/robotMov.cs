ec5c45118f8e847e5fdd3e822740040cc8f23ed0
Memnyatta <shumaylov2000@gmail.com>
2023-07-03 13:11:35 +0300

    Added robot

---
+++ Assets/Characters/robotDel/robotMov.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class robotMov : MonoBehaviour
+{
+    public float speed;
+    public string uterName;
+    [Header("Для просмотра")]
+    public GameObject uter;
+    public NavMeshAgent navAgent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        uter = GameObject.Find(uterName);
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.speed = speed;
+        
+    }
+    public virtual void setDest(Vector3 v) 
+    {
+        navAgent.SetDestination(v);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
