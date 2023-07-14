f0f1fc5a09fa3e02421cb03ca699df3c725c07c0
Memnyatta <shumaylov2000@gmail.com>
2023-07-14 15:14:21 +0300

    More on robots

--- Assets/Characters/robotDelivery/pickableRobotDel.cs
+++ Assets/Characters/robotDelivery/pickableRobotDel.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickableRobotDel : MonoBehaviour
+public class pickableRobotDel : MonoBehaviour, IThrowable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float dragDurSpin;
+    public float spinClosingForce;
 
-    // Update is called once per frame
-    void Update()
+    public List<string> plTags;
+    public string spinBool;
+    public string playerName;
+    [Header("Для просмотра")]
+    public bool isSpinning;
+    public GameObject uter;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Awake()
     {
+        uter = GameObject.Find(playerName);
+        anim = uter.GetComponent<Animator>();
+    }
+    public void startSpin() 
+    {
+        anim.SetBool(spinBool, true);
+        isSpinning = true;
+    }
+    public void stopSpin()
+    {
+        anim.SetBool(spinBool, false);
+        isSpinning = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {    
+         if (plTags.Contains(other.gameObject.tag) && !anim.GetBool(spinBool))
+        {
+                startSpin();
+        }
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        while(anim.GetBool(spinBool) && isSpinning) 
+        {
         
+        }
     }
 }
