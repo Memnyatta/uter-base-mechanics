64907dee23ab635ad86a4b88a06791a3ffe48751
Memnyatta <shumaylov2000@gmail.com>
2023-07-18 21:03:51 +0300

    More on throwing a robot

--- Assets/Characters/robotDelivery/pickableRobotDel.cs
+++ Assets/Characters/robotDelivery/pickableRobotDel.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class pickableRobotDel : MonoBehaviour, IThrowable
 {
+    
     public float dragDurSpin;
     public float spinClosingForce;
     public float rotateSpeed;
     public float dragOffset;
     public float yOffset;
-    public List<string> plTags;
     public string spinBool;
     public string playerName;
+    public string arrowOName;
+    public List<string> plTags;
+    public List<string> enemyTags;
     [Header("Для просмотра")]
     public Rigidbody rb;
+    public bool canDealDam;
     public bool canSpin;
     public bool isSpinning;
-    public GameObject uter;
     public Animator anim;
-
+    public GameObject uter;
+    public GameObject arrowObj;
     // Start is called before the first frame update
     void Awake()
     {
         uter = GameObject.Find(playerName);
         anim = uter.GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        arrowObj = GameObject.Find(arrowOName);
     }
     public void startSpin() 
     {
@@ -33,8 +38,13 @@ public class pickableRobotDel : MonoBehaviour, IThrowable
     }
     public void stopSpin()
     {
+        isSpinning = false;
         anim.SetBool(spinBool, false);
         
+    }
+    public void throwCorpse() 
+    {
+    
     }
     private void OnTriggerEnter(Collider other)
     {    
@@ -48,18 +58,36 @@ public class pickableRobotDel : MonoBehaviour, IThrowable
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (isSpinning && Input.GetButtonUp("Fire1"))
+        {
+            stopSpin();
+        }
         if (canSpin && Input.GetButtonUp("Fire1")) 
         {
             startSpin();
         }
+        
         if (anim.GetBool(spinBool) && isSpinning) 
         {
+            arrowMove();
+
             uter.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self);
 
             Vector3 dragToPos = uter.transform.position + new Vector3(0, yOffset, 0) + uter.transform.forward * dragOffset;
             rb.AddForce((dragToPos - transform.position) * spinClosingForce, ForceMode.VelocityChange);
         }
         
+        
+    }
+    public void arrowMove() 
+    {
+        arrowObj.transform.position = uter.transform.position;
+        Vector3 arrowFrwrd;
+        arrowFrwrd = uter.transform.position - new Vector3(transform.position.x, uter.transform.position.y, transform.position.z);
+        arrowObj.transform.forward = arrowFrwrd;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
         
     }
 }
